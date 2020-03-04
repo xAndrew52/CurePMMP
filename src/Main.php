b6f33fbb1ad94009cad536668e10f6d6caf71740
xAndrei52 <60274436+xAndrei52@users.noreply.github.com>
2020-03-04 19:59:48 +0200

    Create Main.php

---
+++ src/Main.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Cure;
+
+use\pocketmine\Server;
+use\pocketmine\Player;
+use\pocketmine\PluginBase;
+
+use\pocketmine\command{Command, CommandSender};
+
+class Main extends PluginBase{
+
+     public function onEnable(){
+     }
+     
+     public function onCommand(CommandSender $player, Command $cmd, string $label, array $args) :bool {
+     
+           switch($cmd->getName()){
+           case "cure":
+                  if($player instanceof Player){
+                        $player->sendMessage(TextFormat::AQUA . "You have been cured succesfully!");
+                        $player->setHealth(20);
+                        $player->setFood(20);
+                 }
+                 break;
+              }
+           return true;
+           
+     }
+}
